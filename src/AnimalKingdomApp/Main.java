32c97f56633bc7cf5cfb6e05735e746a1cd4c6ab
Khalil <khalilfoulks@gmail.com>
2021-05-20 17:46:21 -0400

    renamed app

--- src/AnimalKingdomApp/Main.java
+++ src/AnimalKingdomApp/Main.java
@@ -1,6 +1,8 @@
-package AnimalKingdomApp;
+package animalKingdomApp;
 public class Main {
     public static void main(String[] args) {
-        
+        Mammals myMammal = new Mammals("Panda", 1869);
+        System.out.println(myMammal.toString());
+
     }
 }
