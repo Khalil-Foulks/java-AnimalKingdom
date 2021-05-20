5637a82219b7aa45dd1f5d6be2c6a447ef2c11a0
Khalil <khalilfoulks@gmail.com>
2021-05-20 19:34:34 -0400

    refactored code and added filters

--- src/AnimalKingdomApp/Main.java
+++ src/AnimalKingdomApp/Main.java
@@ -2,41 +2,101 @@ package animalKingdomApp;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Animal> animalList = new ArrayList<Animal>();
-        animalList.add(new Mammals("Panda", 1869));
-        animalList.add(new Mammals("Zebra", 1778));
-        animalList.add(new Mammals("Koala", 1816));
-        animalList.add(new Mammals("Sloth", 1804));
-        animalList.add(new Mammals("Armadillo", 1758));
-        animalList.add(new Mammals("Raccoon", 1758));
-        animalList.add(new Mammals("Bigfoot", 2021));
+    public static List<Animal> animalList = new ArrayList<Animal>();
 
-        animalList.add(new Birds("Pigeon",1837));
-        animalList.add(new Birds("Peacock",1821));
-        animalList.add(new Birds("Toucan",1758));
-        animalList.add(new Birds("Parrot",1824));
-        animalList.add(new Birds("Swan",1758));
-
-
-        animalList.add(new Fish("Salmon",1758));
-        animalList.add(new Fish("Catfish",1817));
-        animalList.add(new Fish("Perch",1758));
-
-        //sort by year; desc
-        animalList.sort( (Animal a1, Animal a2) -> {
-            return (a1.getYear() > a2.getYear() ? 1: -1);
-        });
+    public static void makeAnimals(){
+        //Mammals
+        Mammal panda = new Mammal("Panda", 1869);
+        animalList.add(panda);
+        Mammal zebra = new Mammal("Zebra", 1778);
+        animalList.add(zebra);
+        Mammal koala = new Mammal("Koala", 1816);
+        animalList.add(koala);
+        Mammal sloth = new Mammal("Sloth", 1804);
+        animalList.add(sloth);
+        Mammal armadillo = new Mammal("Armadillo", 1758);
+        animalList.add(armadillo);
+        Mammal raccoon = new Mammal("Raccoon", 1758);
+        animalList.add(raccoon);
+        Mammal bigfoot = new Mammal("Bigfoot", 2021);
+        animalList.add(bigfoot);
+    
+        //Birds
+        Bird pigeon = new Bird("Pigeon", 1837);
+        animalList.add(pigeon);
+        Bird peacock = new Bird("Peacock", 1821);
+        animalList.add(peacock);
+        Bird toucan = new Bird("Toucan", 1758);
+        animalList.add(toucan);
+        Bird parrot = new Bird("Parrot", 1824);
+        animalList.add(parrot);
+        Bird swan = new Bird("Swan", 1758);
+        animalList.add(swan);
+    
+        //Fish
+        Fish salmon = new Fish("Salmon", 1758);
+        animalList.add(salmon);
+        Fish catfish = new Fish("Catfish", 1817);
+        animalList.add(catfish);
+        Fish perch = new Fish("Perch", 1758);
+        animalList.add(perch);
+    }
+    
+    public static void sortAnimalsYear(){
+        animalList.sort((item1, item2) -> item2.getYear() - item1.getYear());
         System.out.println(animalList);
+    }
 
-        //sort by name; asc
-        animalList.sort( (Animal a1, Animal a2) -> {
-            return (a1.getName().compareToIgnoreCase(a2.getName()));
-        });
+    public static void sortAnimalsAlphabetically(){
+        animalList.sort((item1, item2) -> item1.getName().compareToIgnoreCase(item2.getName()));
         System.out.println(animalList);
+    }
 
-        // Mammals myMammal = new Mammals("Panda", 1869);
-        // System.out.println(myMammal.toString());
+    public static void sortAnimalsByMove(){
+        animalList.sort((item1, item2) -> item1.move().compareToIgnoreCase(item2.move()));
+        System.out.println(animalList);
+    }
 
+    public static List<Animal> filteredList = new ArrayList<>();
+
+    public static void sortAnimals(List<Animal> animals, CheckAnimal tester, boolean printBool){
+        filteredList.clear();
+
+        for (Animal eachAnimal: animals){//for every item in the Animal arrayList
+            if (tester.test(eachAnimal)){//if the test parameter returns true
+                if (printBool){//print if the printBool param is true
+                    System.out.println(eachAnimal.getName() + " " + eachAnimal.reproduce() + " " + eachAnimal.move() + " " + eachAnimal.breathe() + " " + eachAnimal.getYear());
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        makeAnimals();// construct the animals
+    
+        System.out.println("\n*** Sorted by Year ***");
+        sortAnimalsYear();
+    
+        System.out.println("\n*** Sorted Alphabetically ***");
+        sortAnimalsAlphabetically();
+    
+        System.out.println("\n*** Sorted by Movement ***");
+        sortAnimalsByMove();
+    
+        System.out.println("\n*** Animals With Lungs ***");
+        sortAnimals(animalList, each -> each.breathe() == "lungs", true);
+    
+        System.out.println("\n*** Animals With Lungs and From 1758 ***");
+        sortAnimals(animalList, each -> (each.breathe() == "lungs" && each.getYear() == 1758), true);
+    
+        System.out.println("\n*** Animals With Eggs and Lungs ***");
+        sortAnimals(animalList, each -> (each.reproduce() == "eggs" && each.breathe() == "lungs"), true);
+    
+        System.out.println("\n*** Animals from 1758  ***");
+        animalList.sort((item1, item2) -> item1.getName().compareToIgnoreCase(item2.getName()));
+        sortAnimals(animalList, each -> each.getYear() == 1758, true);
+    
+        System.out.println("\n*** Stretch Goal ***");
+        sortAnimals(animalList, each -> each instanceof Mammal, true);
     }
 }
