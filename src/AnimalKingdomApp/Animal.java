57d41f01c3d1650ea084bfd97a7ee5cbb3d26fcb
Khalil <khalilfoulks@gmail.com>
2021-05-20 17:15:50 -0400

    corrected animal class

--- src/AnimalKingdomApp/Animal.java
+++ src/AnimalKingdomApp/Animal.java
@@ -1,11 +1,41 @@
 package AnimalKingdomApp;
 
-public interface Animal {
-    int getId();
-    String getName();
-    int getYear();
+public abstract class Animal {
+    protected static int maxId = 1;
+    protected int id;
+    protected String name;
+    protected int year;
 
-    String move();
-    String breathe();
-    String reproduce();
+    //Constructor
+    public Animal(String name, int year){
+        id = maxId;
+        maxId++;
+        this.name = name;
+        this.year = year;
+    }
+
+    //GETTERS AND SETTERS
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    abstract String move();
+    abstract String breathe();
+    abstract String reproduce();
 }
